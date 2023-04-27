865257e28951197b1da80138eefc9db536df9434
Liga Avotina <liiga.avotina@gmail.com>
2023-04-27 20:10:55 +0300

    create findLongestWord method
    Created findLongestWord(), for finding the longest and shortest word in a file.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        // Write a program to find the longest and shortest word in a file
 
- // Sort text(a-z), before copy to new file, and save that file in different location
+        findLongestWord();
 
-    sortText();
+        // Sort text(a-z), before copy to new file, and save that file in different location
 
+        sortText();
 
+        // Get input from terminal with scanner and add to txt file
 
-// Get input from terminal with scanner and add to txt file
+        getAndAddText();
 
-     getAndAddText();
 
     }
 
 
-    public static void sortText(){
+    public static void sortText() {
         BufferedReader in = null;
         BufferedWriter out = null;
 
@@ -77,20 +79,19 @@ public class Main {
 
         System.out.println("\nEnter your name: ");
         String name = scanner.nextLine();
-        System.out.println("Name: "+ name);
+        System.out.println("Name: " + name);
 
         System.out.println("\nEnter your age: ");
         String age = scanner.nextLine();
-        System.out.println("Age: "+ age);
+        System.out.println("Age: " + age);
 
         System.out.println("\nEnter your gender: ");
         String gender = scanner.nextLine();
-        System.out.println("Gender: "+ gender);
+        System.out.println("Gender: " + gender);
 
         System.out.println("\nEnter your profession: ");
         String profession = scanner.nextLine();
-        System.out.println("Profession: "+ profession);
-
+        System.out.println("Profession: " + profession);
 
 
         FileOutputStream out = null;
@@ -145,4 +146,56 @@ public class Main {
             }
         }
     }
+
+
+// Track the Strings
+// Use FileReader, begin to read each character
+// Check for spaces, when find space, it means find end of word (\s - for space)
+// Keep recording characters until find space
+// Check the length, if it's longer, than stored as longest;
+// Set temp.String empty again and keep searching
+    public static void findLongestWord() {
+
+        FileReader in = null;
+
+        try {
+            in = new FileReader("src/main/java/org/example/Files/file3.txt");
+            int c;
+            String longest = "";
+            String shortest = "";
+
+            StringBuilder temp = new StringBuilder();
+            while ((c = in.read()) != -1) {
+                while ((char) c != ' ' && c != -1) {
+                    temp.append((char) c);
+                    c = in.read();
+                }
+
+                if (longest.isEmpty() || temp.length() >= longest.length()) {
+                    longest = temp.toString();
+                } else if (shortest.isEmpty() || temp.length() <= shortest.length()) {
+                    shortest = temp.toString();
+//                    if (temp.length() == shortest.length()) {
+//                        System.out.println(shortest);
+                    }
+
+                temp.setLength(0); // clear temp, when find one word
+            }
+
+            System.out.println("\nLast founded longest word: " + longest);
+            System.out.println("Last founded shortest word: " + shortest);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
